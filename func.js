d274e694789a90cd3f2b1cd7123036d1522b05eb
pauman_vslabs <pauman@react.vslabs.com>
2024-02-01 13:13:16 +0200

    new commit

--- func.js
+++ func.js
@@ -76,3 +76,27 @@ var twoSum = function(nums, target) {
 let two = twoSum([2, 7, 11, 15], 9);
 console.log(two)
 
+// --------------------------------------------------------------------------
+
+// Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+// toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
+// notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
+ 
+// Example 1:
+
+// Input: func = () => expect(5).toBe(5)
+// Output: {"value": true}
+// Explanation: 5 === 5 so this expression returns true.
+
+var expect = function(val) {
+  return {
+      toBe:(val2) => {
+          if(val !== val2 ) throw Error("Not Equal");
+          else return true
+      },
+      notToBe:(val2)=>{
+if(val === val2) throw Error("Equal");
+else return true
+      }
+  }
+};
